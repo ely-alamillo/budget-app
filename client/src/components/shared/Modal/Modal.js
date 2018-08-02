9e81b2b36396f83e03ad2584acb2f561e4f54cf3
Ely Alamillo <ely0013@gmail.com>
2018-08-02 16:56:21 -0500

    created modal component

--- client/src/components/shared/Modal/Modal.js
+++ client/src/components/shared/Modal/Modal.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const Modal = props => {
+export const Modal = props => {
+  const toggle = () => {
+    console.log(document.querySelector(props.for));
+    document.getElementById(props.for).classList.toggle('is-active');
+  };
+
   return (
-    <div class="modal">
-      <div class="modal-background" />
-      <div class="modal-content">
-        <h1>Title</h1>
+    <div className="modal" id={props.for}>
+      <div className="modal-background" />
+      <div className="modal-content">
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{props.title}</p>
+            <button className="delete" aria-label="close" onClick={toggle} />
+          </header>
+          <section className="modal-card-body">Enter Info</section>
+          <footer className="modal-card-foot">
+            <button className="button is-success">Create</button>
+            <button className="button" onClick={toggle}>
+              Cancel
+            </button>
+          </footer>
+        </div>
       </div>
-      <button class="modal-close is-large" aria-label="close" />
     </div>
   );
 };
-
-export default Modal;
