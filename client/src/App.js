e8ee03a739b6c2034ddc38489938f9ed9aa2e005
Ely Alamillo <ely0013@gmail.com>
2018-08-01 22:47:31 -0500

    added inital routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
+// import Home from './components/home/home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello from react</h1>
+      <div>
+        <Navigation />
+        {/* <Route path="/" exact={true} component={Home} /> */}
+        <Footer />
       </div>
     );
   }
