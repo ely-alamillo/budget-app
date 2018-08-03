b052d8133ecd11d7724059496528ea1cbe8ea1d1
Ely Alamillo <ely0013@gmail.com>
2018-08-03 11:04:52 -0500

    created dahsboard component

---
+++ client/src/components/Home/Dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class Dasboard extends Component {
+  render() {
+    return (
+      <div>
+        <div className="tile is-ancestor">
+          <div className="tile">
+            <div className="tile">
+              <div className="tile is-parent ">
+                <article className="tile is-child notification is-success">
+                  <p className="title">Budget</p>
+                </article>
+              </div>
+            </div>
+            <div className="tile">
+              <div className="tile is-parent ">
+                <article className="tile is-child notification is-warning">
+                  <p className="title">Expenses</p>
+                </article>
+              </div>
+            </div>
+            <div className="tile">
+              <div className="tile is-parent ">
+                <article className="tile is-child notification is-warning">
+                  <p className="title">Categories</p>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="tile is-ancestor">
+          <div className="tile">
+            <div className="tile">
+              <div className="tile is-parent ">
+                <article className="tile is-child notification is-primary">
+                  <p className="title">graphs here</p>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dasboard;
