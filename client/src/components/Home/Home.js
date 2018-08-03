d6f1cba6775afbc231173a0a65e85ca0f17a5342
Ely Alamillo <ely0013@gmail.com>
2018-08-03 17:57:33 -0500

    calculating stats - wip

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -8,7 +8,7 @@ import Dashboard from './Dashboard/Dashboard';
 import './Home.css';
 
 class Home extends Component {
-  state = { budgets: {}, selectedBudget: {} };
+  state = { budgets: {}, selectedBudget: {}, stats: {} };
 
   createBudget = budget => {
     console.log('set create budget');
@@ -20,18 +20,30 @@ class Home extends Component {
     const { budgetName } = this.state.selectedBudget;
     const { expenses } = this.state.budgets[budgetName];
 
-    this.setState({
-      budgets: {
-        [budgetName]: {
-          ...this.state.budgets[budgetName],
-          expenses: [...expenses, expense]
+    this.setState(
+      {
+        budgets: {
+          [budgetName]: {
+            ...this.state.budgets[budgetName],
+            expenses: [...expenses, expense]
+          }
+        },
+        selectedBudget: {
+          ...this.state.selectedBudget,
+          expenses: [...this.state.selectedBudget.expenses, expense]
         }
       },
-      selectedBudget: {
-        ...this.state.selectedBudget,
-        expenses: [...this.state.selectedBudget.expenses, expense]
-      }
-    });
+      () => this.calculateStats()
+    );
+  };
+
+  calculateStats = () => {
+    console.log('calculatin');
+    // const { selectedBudget } = this.state;
+
+    console.log(this.state.selectedBudget.expenses);
+
+    // this.setState({ stats: { expensesTotal } });
   };
 
   render() {
