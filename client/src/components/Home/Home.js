d8cfd99d8037a563d79ebb505503a3de1cd8196c
Ely Alamillo <ely0013@gmail.com>
2018-08-03 11:05:47 -0500

    removed dashboard items and created Dashboard to replace them

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,52 +3,30 @@ import { Link, NavLink } from 'react-router-dom';
 import { Component } from 'react';
 
 import ButtonToolbar from './ButtonToolbar/ButtonToolbar';
+import Dashboard from './Dashboard/Dashboard';
 
 import './Home.css';
 
 class Home extends Component {
+  state = { budgets: {} };
+
+  createBudget = budget => {
+    console.log('set create budget');
+    const name = budget.budgetName;
+    this.setState({ budgets: { [name]: budget } });
+  };
+
+  addNewExpense = expense => {
+    console.log('add expense');
+  };
   render() {
     return (
       <section className="section" style={{ height: '90vh' }}>
-        <ButtonToolbar />
-
-        <div className="tile is-ancestor">
-          <div className="tile">
-            <div className="tile">
-              <div className="tile is-parent ">
-                <article className="tile is-child notification is-success">
-                  <p className="title">Budget</p>
-                </article>
-              </div>
-            </div>
-            <div className="tile">
-              <div className="tile is-parent ">
-                <article className="tile is-child notification is-warning">
-                  <p className="title">Expenses</p>
-                </article>
-              </div>
-            </div>
-            <div className="tile">
-              <div className="tile is-parent ">
-                <article className="tile is-child notification is-warning">
-                  <p className="title">Categories</p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="tile is-ancestor">
-          <div className="tile">
-            <div className="tile">
-              <div className="tile is-parent ">
-                <article className="tile is-child notification is-primary">
-                  <p className="title">graphs here</p>
-                </article>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ButtonToolbar
+          createBudget={this.createBudget}
+          addNewExpense={this.addNewExpense}
+        />
+        <Dashboard />
       </section>
     );
   }
