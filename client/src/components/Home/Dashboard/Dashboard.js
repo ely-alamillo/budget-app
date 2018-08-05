e516de747df3f72ecc827939b847f04c115e958a
Ely Alamillo <ely0013@gmail.com>
2018-08-04 20:07:37 -0500

    adde chart and fake data

--- client/src/components/Home/Dashboard/Dashboard.js
+++ client/src/components/Home/Dashboard/Dashboard.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import LineChart from '../Charts/LineChart/LineChart';
+import './Dashboard.css';
+import RadarChart from '../Charts/RadarChart/RadarChart';
 
 class Dashboard extends Component {
   render() {
@@ -45,6 +48,50 @@ class Dashboard extends Component {
               <div className="tile is-parent ">
                 <article className="tile is-child notification is-warning">
                   <p className="subtitle">Transactions</p>
+                  <div className="table-custom">
+                    <table className="table is-fullwidth">
+                      <thead>
+                        <tr>
+                          <th>Firstname</th>
+                          <th>Lastname</th>
+                          <th>Age</th>
+                        </tr>
+                      </thead>
+
+                      <tbody>
+                        <tr>
+                          <td>Jill</td>
+                          <td>Smith</td>
+                          <td>50</td>
+                        </tr>
+                        <tr>
+                          <td>Eve</td>
+                          <td>Jackson</td>
+                          <td>94</td>
+                        </tr>
+                        <tr>
+                          <td>Eve</td>
+                          <td>Jackson</td>
+                          <td>94</td>
+                        </tr>
+                        <tr>
+                          <td>Eve</td>
+                          <td>Jackson</td>
+                          <td>94</td>
+                        </tr>
+                        <tr>
+                          <td>Eve</td>
+                          <td>Jackson</td>
+                          <td>94</td>
+                        </tr>
+                        <tr>
+                          <td>Eve</td>
+                          <td>Jackson</td>
+                          <td>94</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
                 </article>
               </div>
             </div>
@@ -56,7 +103,17 @@ class Dashboard extends Component {
             <div className="tile">
               <div className="tile is-parent ">
                 <article className="tile is-child notification is-primary">
-                  <p className="title">graphs here</p>
+                  <p className="title">Budget and Expenses</p>
+                  <LineChart />
+                </article>
+              </div>
+            </div>
+
+            <div className="tile">
+              <div className="tile is-parent ">
+                <article className="tile is-child notification is-primary">
+                  <p className="title">Spending by Category</p>
+                  <RadarChart data={this.props.radarChartData} />
                 </article>
               </div>
             </div>
