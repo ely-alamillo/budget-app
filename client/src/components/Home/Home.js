d5dd50c556332ae1718e6e5e7a7a60c405a2dbad
Ely Alamillo <ely0013@gmail.com>
2018-08-04 20:08:05 -0500

    added fake state data

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,10 +6,116 @@ import ButtonToolbar from './ButtonToolbar/ButtonToolbar';
 import Dashboard from './Dashboard/Dashboard';
 
 import './Home.css';
-import { access } from 'fs';
+import { totalmem } from 'os';
 
+const testState = {
+  budgets: {
+    one: {
+      budgetName: 'one',
+      budgetTotal: '10000',
+      category: '',
+      description: 'Monthly Budget',
+      expenses: [
+        {
+          category: 'Other',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Other',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Rent',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Clothing',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        }
+      ]
+    }
+  },
+  selectedBudget: {
+    budgetName: 'one',
+    budgetTotal: '10000',
+    category: '',
+    description: 'Monthly Budget',
+    expenses: [
+      {
+        category: 'Other',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Other',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Rent',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Clothing',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      }
+    ]
+  },
+  stats: { expensesTotal: 700 }
+};
 class Home extends Component {
-  state = { budgets: {}, selectedBudget: {}, stats: {} };
+  // state = { budgets: {}, selectedBudget: {}, stats: {} };
+  state = testState;
 
   createBudget = budget => {
     console.log('set create budget');
@@ -44,12 +150,31 @@ class Home extends Component {
     const expensesTotal = selectedBudget.expenses.reduce(getTotal, 0);
     console.log(this.state.selectedBudget.expenses);
 
-    this.setState({ stats: { expensesTotal } });
+    this.setState({ stats: { expensesTotal } }, () =>
+      this.calculateSpendingByCat()
+    );
+  };
+
+  calculateSpendingByCat = () => {
+    const { selectedBudget } = this.state;
+    const getTotal = (total, expense) => {
+      const { category, expenseTotal } = expense;
+
+      // total[category] != undefined
+      //   ? (total[category] = Number(expenseTotal) + total[category])
+      //   : (total[category] = Number(expenseTotal));
+      total[category] =
+        Number(expenseTotal) + total[category] || Number(expenseTotal);
+
+      return total;
+    };
+    const categoryTotal = selectedBudget.expenses.reduce(getTotal, {});
+    this.setState({ stats: { ...this.state.stats, categoryTotal } });
   };
 
   render() {
     return (
-      <section className="section" style={{ height: '90vh' }}>
+      <section className="section" style={{ minHeight: '90vh' }}>
         <ButtonToolbar
           createBudget={this.createBudget}
           addExpense={this.addExpense}
@@ -57,6 +182,7 @@ class Home extends Component {
         <Dashboard
           budget={this.state.selectedBudget}
           stats={this.state.stats}
+          radarChartData={this.state.stats.categoryTotal}
         />
       </section>
     );
