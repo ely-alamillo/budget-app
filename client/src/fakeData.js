05acff1785ac9f46ab12aafceb0874051a2ef89c
Ely Alamillo <ely0013@gmail.com>
2018-08-06 16:43:17 -0500

    moved fake data to own file

---
+++ client/src/fakeData.js
@@ -0,0 +1,105 @@
+export const testState = {
+  budgets: {
+    one: {
+      budgetName: 'one',
+      budgetTotal: '10000',
+      category: '',
+      description: 'Monthly Budget',
+      expenses: [
+        {
+          category: 'Other',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Other',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Rent',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Clothing',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        },
+        {
+          category: 'Food',
+          description: 'coffee',
+          expenseTotal: '100',
+          expenseName: 'Starbucks'
+        }
+      ]
+    }
+  },
+  selectedBudget: {
+    budgetName: 'one',
+    budgetTotal: '10000',
+    category: '',
+    description: 'Monthly Budget',
+    expenses: [
+      {
+        category: 'Other',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Other',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Rent',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Clothing',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      },
+      {
+        category: 'Food',
+        description: 'coffee',
+        expenseTotal: '100',
+        expenseName: 'Starbucks'
+      }
+    ]
+  },
+  stats: { expensesTotal: 700 }
+};
