781f155f30dc1038c6f6183165162d8d2bae5374
Ely Alamillo <ely0013@gmail.com>
2018-08-06 20:11:02 -0500

    removed data

--- client/src/components/Home/Charts/RadarChart/RadarChart.js
+++ client/src/components/Home/Charts/RadarChart/RadarChart.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import { Radar } from 'react-chartjs-2';
 
-const data = {
-  labels: ['Food', 'Rent', 'Clothing', 'Other', 'Subscriptions'],
-  datasets: [
-    {
-      label: 'Total Spending',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      pointBackgroundColor: 'rgba(255,99,132,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(255,99,132,1)',
-      data: [28, 48, 40, 19, 55]
-    }
-  ]
-};
 const isNil = x => {
   return x === undefined || x === null;
 };
